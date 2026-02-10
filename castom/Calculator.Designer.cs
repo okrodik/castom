4d92aa3ff452228105e8917aedfd57323810507e
student1 <student1@class.local>
2026-02-10 13:09:39 +0900

    начал делать калькулятор

--- castom/Calculator.Designer.cs
+++ castom/Calculator.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace castom
 {
-    partial class Form1
+    partial class Calculator
     {
-        /// <summary>
+        /// <summary> 
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Освободить все используемые ресурсы.
         /// </summary>
         /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
@@ -20,42 +20,18 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Код, автоматически созданный конструктором компонентов
 
-        /// <summary>
+        /// <summary> 
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.calculator1 = new castom.Calculator();
-            this.SuspendLayout();
-            // 
-            // calculator1
-            // 
-            
-            this.calculator1.Location = new System.Drawing.Point(39, 13);
-            this.calculator1.Name = "calculator1";
-           
-            this.calculator1.Size = new System.Drawing.Size(726, 425);
-            this.calculator1.TabIndex = 0;
-  
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.calculator1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private Calculator calculator1;
     }
 }
-
---
+++ castom/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using castom.Properties;
+
+namespace castom
+{
+    public partial class Calculator : UserControl
+    {
+        private TextBox pictureBox;
+
+        private readonly Button[] buttons;
+
+        public Calculator()
+        {
+            buttons = CreateButtonArray();
+            AddControlsToPanel();
+        }
+
+        private Button[] CreateButtonArray()
+        {
+            Button[] buttons = new Button[4];
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new Button
+                {
+                    Name = $"button{i + 1}",
+                    AutoSize = true
+                };
+            }
+            return buttons;
+        }
+
+
+        private void AddControlsToPanel()
+        {
+            int xxx = 10;
+            foreach (Button btn in buttons)
+            {
+                btn.Location = new Point(10 + xxx, 10 + xxx);
+                this.Controls.Add(btn);
+            }
+        }
+
+        [Category("Внешний вид"), Description("Цвет фона карточки")]
+        public override Color BackColor
+        {
+            get => base.BackColor;
+            set
+            {
+                this.BackColor = value;
+            }
+        }
+
+        protected virtual void OnButtonClick(object sender, EventArgs e)
+        {
+        }
+    }
+}
